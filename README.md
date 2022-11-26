8ac26a99211d9005c50d0ec82fce8dfcc8dcd4b3
Olly de Sousa <0lly@protonmail.com>
2022-11-26 13:52:54 +0000

    Add rough volatility param to price.
    It's not that accurate as usually volatility is persistant over a period of time, but this just adds some random volatility to an individual price point.

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 The following binary can be used to create dummy historical data for various FX crosses.
 I created this to practice my rust and to generate constantly changing prices for an FX Trading game I am working on. 
 
+Disclaimer - This _should not_ be used for anything that requires accuracy like backtesting an algorithm ect.
+It's use is intended solely for creating games or having fun.
+
 ## Arguments
 The binary requires you to pass 2 arguments.
 
--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,22 @@ fn main() -> Result<(), &'static str> {
     }
 
     let ccy_arg: &str = args[1].as_str();
+    let num_trades: i32 = (&args[2]).parse::<i32>().unwrap();
     let ccy: f64 = match ccy_arg {
         "GBP/USD" => 1.21050,
         "EUR/USD" => 1.04262,
         "EUR/GBP" => 0.86670,
         _ => panic!("Invalid currency pair. Please see docs for instructions.")
     };
-
     println!("CCY PRICE: {}: {}",ccy_arg, ccy);
 
-    let num_trades: i32 = (&args[2]).parse::<i32>().unwrap();
     let prices = create_next_x_prices(ccy, num_trades);
-
     println!("Total: {} \nData: {:?}",prices.len(), prices);
 
     Ok(())
 }
 
-/// Given a currency, it will simulate the next x prices.
+/// Given a currency, it will simulate x historical prices.
 fn create_next_x_prices(base: f64, number_to_generate: i32) -> Vec<f64> {
     let mut i: i32 = 0;
     let mut curr_price: f64 = base;
@@ -36,16 +34,8 @@ fn create_next_x_prices(base: f64, number_to_generate: i32) -> Vec<f64> {
     while i < number_to_generate {
         i += 1;
 
-        // generates a random float between 0 - 20
-        let rng: f64 = thread_rng().gen_range(0.0..20.0);
-        // generates a random boolean
-        let is_addition: bool = random();
-
-        let new_price: f64 = if is_addition {
-            &curr_price + (&curr_price * (rng / 100000.0))
-        } else {
-            &curr_price - (&curr_price * (rng / 100000.0))
-        };
+        let price_movement: f64 = determine_price_move();
+        let new_price: f64 = determine_price_direction(&curr_price, price_movement);
 
         // round the numbers to 5 D.P.
         let rounded_to_five_dp: f64 = (new_price * 100000f64).floor() / 100000.0;
@@ -55,4 +45,32 @@ fn create_next_x_prices(base: f64, number_to_generate: i32) -> Vec<f64> {
     }
 
     return array;
+}
+
+/// Determines the price movement.
+/// Calculates price move and adds random chance of x standard deviation move.
+fn determine_price_move() -> f64 {
+    // firstly let's determine the sigma of the move.
+    // This is a good primer on sigma from MIT: https://tinyurl.com/8ecf5ud5
+    let sigma_range: f64 = thread_rng().gen_range(0.0..100.0);
+    let sigma: i32 =
+        if sigma_range < 68.2 { 1 }
+        else if sigma_range > 68.2 && sigma_range < 95.8 { 2 }
+        else if sigma_range > 95.8 && sigma_range < 0.2 { 3 }
+        else if sigma_range > 0.2 && sigma_range < 0.000001 { 4 }
+        else { 5 };
+
+    let rng: f64 = thread_rng().gen_range(0.0..10.0);
+
+    return rng * sigma as f64;
+}
+
+/// Given a price and price movement,
+/// it will randomly determine whether the price movement is positive or negative.
+fn determine_price_direction(curr_price: &f64, price_movement: f64) -> f64 {
+    let is_addition: bool = random();
+
+    return
+        if is_addition { curr_price + (*&curr_price * (price_movement / 100000.0)) }
+        else { curr_price - (*&curr_price * (price_movement / 100000.0)) };
 }
